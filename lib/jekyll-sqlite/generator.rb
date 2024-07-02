86a76d8e36f529696d324bc6132d4cbeb6bb91e4
Nemo <me@captnemo.in>
2024-07-02 17:11:02 +0530

    fixes to get it functional
    The code was never tested properly, and broken
    during the migration from a script to a gem.
    
    This fixes all of that
    
    - Fixes #1
    - Closes #2

--- lib/jekyll-sqlite/generator.rb
+++ lib/jekyll-sqlite/generator.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
-require 'sqlite3'
+require "sqlite3"
+
 module JekyllSQlite
   # Main generator class
   class Generator < Jekyll::Generator
@@ -14,22 +15,22 @@ module JekyllSQlite
     end
 
     # Get the root of where we are generating the data
-    def get_root(root, name)
-      name.split(".")[0..-2].each do |p|
+    def get_root(root, db_name)
+      db_name.split(".")[0..-2].each do |p|
         root = root[p]
       end
       root
     end
 
-    def gen_hash_data(root, db, _query)
+    def gen_hash_data(root, db, db_name, query)
       root ||= {}
 
-      root[name] = db.execute(config["query"])
-      root[name].size
+      root[db_name] = db.execute(query)
+      root[db_name].size
     end
 
-    def gen_nested_data(item, db, query, name)
-      item[name] = []
+    def gen_nested_data(item, db, query, db_name)
+      item[db_name] = []
       db.prepare(query) do |stmt|
         # We bind params, ignoring any errors
         # Since there's no way to get required params
@@ -38,30 +39,30 @@ module JekyllSQlite
           stmt.bind_param key, value
         rescue StandardError # rubocop:disable Lint/SuppressedException
         end
-        stmt.execute.each { |d| item[name] << d }
+        stmt.execute.each { |d| item[db_name] << d }
       end
-      item[name].size
+      item[db_name].size
     end
 
-    def array_gen(root, config, name, db)
+    def array_gen(root, config, db_name, db)
       count = 0
       root.each do |item|
         # TODO: Add support for binding Arrays as well.
         if item.is_a? Hash
-          count += gen_nested_data(item, db, config["query"], name)
+          count += gen_nested_data(item, db, config["query"], db_name)
         else
-          Jekyll.logger.info "Jekyll SQLite:", "Item is not a hash for #{name}. Unsupported configuration"
+          Jekyll.logger.info "Jekyll SQLite:", "Item is not a hash for #{db_name}. Unsupported configuration"
         end
       end
       count
     end
 
-    def gen_data(root, config, name, db)
+    def gen_data(root, config, db_name, db)
       count = 0
       if root.nil? || (root.is_a? Hash)
-        count = gen_hash_data(root, db, config["query"])
+        count = gen_hash_data(root, db, db_name, config["query"])
       elsif root.is_a? Array
-        count = array_gen(root, config, name, db)
+        count = array_gen(root, config, db_name, db)
       end
       count
     end
@@ -70,15 +71,28 @@ module JekyllSQlite
       name.split(".")[-1]
     end
 
+    def validate_config(config)
+      return false unless config.is_a? Hash
+      return false unless config.key?("query")
+      return false unless File.exist?(config["file"])
+      return false unless config.key?("data")
+      true
+    end
+
     def generate(site)
-      site.config["sqlite"].each do |config|
-        name = config["data"]
+      gem_config = site.config['sqlite'] || []
+      gem_config.each do |config|
+        unless validate_config(config)
+          Jekyll.logger.error "Jekyll SQLite:", "Invalid Configuration. Skipping"
+          next
+        end
+        d_name = config["data"]
         SQLite3::Database.new config["file"], readonly: true do |db|
           fast_setup db
           db.results_as_hash = config.fetch("results_as_hash", true)
-          root = get_root(site.data, name)
-          count = gen_data(root, config, get_tip(name), db)
-          Jekyll.logger.info "Jekyll SQLite:", "Loaded #{name}. Count=#{count}. as_hash=#{db.results_as_hash}"
+          root = get_root(site.data, d_name)
+          count = gen_data(root, config, get_tip(d_name), db)
+          Jekyll.logger.info "Jekyll SQLite:", "Loaded #{d_name}. Count=#{count}"
         end
       end
     end
